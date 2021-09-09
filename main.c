93045b373614da3e498b9d194cc831c657c0a67a
Sam Kinsella <57734208+kinsellasam@users.noreply.github.com>
2021-09-08 19:45:28 -0500

    Reading A File

--- main.c
+++ main.c
@@ -12,22 +12,26 @@ int main(int argc, char *argv[]){
   while((opts = getopt(argc, argv, "ht:")) != -1){
     switch (opts){
       case 'h':
+        hFlag++;
         fprintf(stderr, "Usage: %s [-h] [-t time] [logfile]\n", argv[0]);
         printf("-h: The help command. This will print the usage prompt.\n");
         printf("-t: This command can accept a number seconds as a following argument.\n");
         printf("\tIf another argument is not given, a default value will be used..\n");
         printf("logfile: The user can specify the logfile.\n");
-        hFlag++;
         return -1;
 
       case 't':
         tFlag++;
-        timeVal = optopt;
+        timeVal = atoi(optarg);
+        if(timeVal < 0 || timeVal > 20){
+          fprintf(stderr, "Option -%c requires an argument [1-20].\n", optopt);
+          return -1;
+        }
         break;
 
       case '?':
         if(optopt == 't'){
-          fprintf(stderr, "Option -%c requires an argument [0-9].\n", optopt);
+          fprintf(stderr, "Option -%c requires an argument [1-20].\n", optopt);
         }
         else if(isprint(optopt)){
           fprintf(stderr, "Unknown option '-%c'.\n", optopt);
@@ -39,6 +43,22 @@ int main(int argc, char *argv[]){
       default:
         abort();
     }
+  }
+
+  if(optind < argc){
+    do{
+      char *filenameArr;
+      filenameArr = argv[optind];
+
+      FILE *fileptr;
+      fileptr = fopen(filenameArr, "r");
+
+      if(fileptr == NULL){
+        printf("File '%s' not found. Exiting.\n", filenameArr);
+        return -1;
+      }
+
+    }while(++optind < argc);
   }
 
   printf("The argument array contains:\n");
